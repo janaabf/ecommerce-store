c24586ee602ce96b1bb98bdba2dbed39160d5d12
Jana Abfalterer <abfalterer.jana@gmail.com>
2022-05-24 15:22:29 +0200

    Add initial setup

---
+++ pages/_app.js
@@ -0,0 +1,32 @@
+import { css, Global } from '@emotion/react';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Global
+        styles={css`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+
+          a {
+            color: inherit;
+            text-decoration: none;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}
+      />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Store</title>
+        <meta
+          name="My first E-Commerce store"
+          content="My first E-Commerce store"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>does it work?</h1>
+      </main>
+    </div>
+  );
+}
