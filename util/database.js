921244625d76a549626d6866e64f4cc004385a32
Jana Abfalterer <abfalterer.jana@gmail.com>
2022-06-27 21:57:45 +0200

    Deploy on Heroku

--- util/database.js
+++ util/database.js
@@ -1,9 +1,29 @@
 import { config } from 'dotenv-safe';
 import postgres from 'postgres';
 
+// deploying on Heroku
+module.exports = function setPostgresDefaultsOnHeroku() {
+  if (process.env.DATABASE_URL) {
+    const { parse } = require('pg-connection-string');
+
+    // Extract the connection information from the Heroku environment variable
+    const { host, database, user, password } = parse(process.env.DATABASE_URL);
+
+    // Set standard environment variables
+    process.env.PGHOST = host;
+    process.env.PGDATABASE = database;
+    process.env.PGUSERNAME = user;
+    process.env.PGPASSWORD = password;
+  }
+};
+
 config(); // to get info from .env for the database
 
-const sql = postgres(); // to use postgres
+// const sql = postgres(); // to use postgres locally
+
+// Heroku needs SSL connections but has an "unauthorized" certificate
+// https://devcenter.heroku.com/changelog-items/852
+const sql = postgres({ ssl: { rejectUnauthorized: false } });
 
 // export data from sql database
 export async function getProductData() {
